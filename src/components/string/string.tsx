8cccf3e1769bbe8bfc3fe8eca7f2897afacbcd33
Denis Ivanov <site.ivanov201@yandex.ru>
2023-09-12 18:19:28 +0300

    Fibonacci

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -6,15 +6,13 @@ import { Button } from '../ui/button/button'
 import { Circle } from '../ui/circle/circle'
 import { TInput, ElementStates } from '../../types/element-states'
 import { swap } from './string-alg'
-import { DELAY_IN_MS } from '../../constants/delays'
+import { DELAY_IN_MS, delay } from '../../constants/delays'
 
 export const StringComponent: FC = () => {
 
   const [inputValue, setInputValue] = useState('')
-  const [loader, setLoader] = useState(false)
+  const [loader, setLoader] = useState<boolean>(false)
   const [stringArr, setStringArr] = useState<TInput[]>([])
-
-  const delay = (time: number) => new Promise(res => setTimeout(res, time))
 
   const handleChange = (e: SyntheticEvent<HTMLInputElement>) => setInputValue(e.currentTarget.value)
 
@@ -31,27 +29,23 @@ export const StringComponent: FC = () => {
 
   const createElement = async (arr: TInput[]) => {
     setLoader(true);
-    const mid = Math.floor(arr.length / 2);
+    const mid = Math.floor(arr.length / 2)
 
     for (let i = 0; i < mid; i++) {
-      let j = arr.length - 1 - i;
-
+      let j = arr.length - 1 - i
       if (i !== j) {
-        arr[i].color = ElementStates.Changing;
-        arr[j].color = ElementStates.Changing;
-        setStringArr([...arr]);
-        await delay(DELAY_IN_MS);
+        arr[i].color = ElementStates.Changing
+        arr[j].color = ElementStates.Changing
+        setStringArr([...arr])
+        await delay(DELAY_IN_MS)
       }
-
-      swap(arr, i, j);
-
-      arr[i].color = ElementStates.Modified;
-      arr[j].color = ElementStates.Modified;
-
-      setStringArr([...arr]);
+      swap(arr, i, j)
+      arr[i].color = ElementStates.Modified
+      arr[j].color = ElementStates.Modified
+      setStringArr([...arr])
     }
-    setLoader(false);
-  };
+    setLoader(false)
+  }
 
   return (
     <SolutionLayout
